a6e5edbb1686135963b8af3fe75ffabeb0f463d9
Matthew Meshulam <meshu003@umn.edu>
2025-03-07 16:30:03 -0600

    Schema fixes

--- config/schema/1.0/patches-bellatlas/sb_schema_patch-bellatlas-consistency.sql
+++ config/schema/1.0/patches-bellatlas/sb_schema_patch-bellatlas-consistency.sql
@@ -16,3 +16,15 @@ DROP TABLE `x_omoccurloans`;
 
 ALTER TABLE `omexsiccatititles`
   CHANGE COLUMN `sourceIdentifier` `sourceIdentifier` varchar(150) DEFAULT NULL;
+
+SET sql_mode = '';
+UPDATE omoccurdeterminations SET dateIdentifiedInterpreted = NULL WHERE dateIdentifiedInterpreted = '0000-00-00';
+
+-- duplicate SELECT * FROM userroles where uid = 206973 and role = 'ClAdmin' and tablename = 'fmchecklists' and tablepk = 645;
+DELETE FROM userroles where userroleid = 1937 limit 1;
+
+UPDATE omoccurrences SET eventDate = NULL WHERE YEAR(eventDate) < 1000;
+UPDATE omoccurrences SET eventDate = CONCAT(YEAR(eventDate), '-01-01') WHERE MONTH(eventDate) = 0;
+UPDATE omoccurrences SET eventDate = CONCAT(YEAR(eventDate), '-', MONTH(eventDate), '-01') WHERE DAY(eventDate) = 0;
+
+UPDATE omoccurdeterminations SET initialTimestamp = CURRENT_TIMESTAMP WHERE YEAR(initialTimestamp) < 1000;
