71262a954fc7c5af5daf5d22568a497b038f75e5
Matthew Meshulam <meshu003@umn.edu>
2025-03-25 11:48:54 -0500

    Add patches for uploadspectemp column types

---
+++ config/schema/3.0/patches-bellatlas/db_schema_patch-bellatlas-custom.sql
@@ -0,0 +1,12 @@
+-- schema changes specific to bell atlas, to be applied after 3.x patches
+
+-- make uploadspectemp column types consistent with omoccurrences
+ALTER TABLE `uploadspectemp` 
+  CHANGE COLUMN `labelProject` `labelProject` VARCHAR(250) DEFAULT NULL,
+  CHANGE COLUMN `taxonRemarks` `taxonRemarks` text,
+  CHANGE COLUMN `identificationReferences` `identificationReferences` text,
+  CHANGE COLUMN `identificationRemarks` `identificationRemarks` text,
+  CHANGE COLUMN `recordNumber` `recordNumber` varchar(45) DEFAULT NULL COMMENT 'Collector Number',
+  CHANGE COLUMN `eventID` `eventID` varchar(150) DEFAULT NULL,
+  CHANGE COLUMN `waterBody` `waterBody` varchar(75) DEFAULT NULL,
+  CHANGE COLUMN `georeferenceRemarks` `georeferenceRemarks` varchar(500) DEFAULT NULL;
