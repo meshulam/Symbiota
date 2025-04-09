meshulam/Symbiota

commit 3ecdff1487d594c3e473c9b69f0c4dd07c799fed

Update exchangetab.php
Sets form field to an initial value of '1' This value is quickly over  written by an ajax request retreiving next loan identifier value from.  By setting this value it provides some context for what type of value would go in this field if the ajax query does not return

collections/loans/exchangetab.php
@@ -24,7 +24,7 @@ if(!$transInstList) echo '<script type="text/javascript">displayNewExchange();</
 			<div style="padding-top:10px;float:left;">
 				<span>
 					<b><?php echo $LANG['TRANS_NO']; ?>:</b>
-					<input type="text" autocomplete="off" name="identifier" maxlength="255" style="width:120px;border:2px solid black;text-align:center;font-weight:bold;color:black;" value="" />
+					<input type="text" autocomplete="off" name="identifier" maxlength="255" style="width:120px;border:2px solid black;text-align:center;font-weight:bold;color:black;" value="1" />
 				</span>
 			</div>
 			<div style="clear:left;padding-top:6px;float:left;">

commit 25b68cab32fb6d1857612a28bfb28a362d52e92d

Bugfix: ensure we fetch next exchange identifier
https://github.com/Symbiota/Symbiota/pull/1074/commits/3ecdff1487d594c3e473c9b69f0c4dd07c799fed introduced a bug where the next exchange/gift ID would always show 1. This is because in the displayNewExchange JS function, it skips the AJAX call when the value isn't blank.

collections/loans/exchangetab.php
@@ -24,7 +24,7 @@ if(!$transInstList) echo '<script type="text/javascript">displayNewExchange();</
 			<div style="padding-top:10px;float:left;">
 				<span>
 					<b><?php echo $LANG['TRANS_NO']; ?>:</b>
-					<input type="text" autocomplete="off" name="identifier" maxlength="255" style="width:120px;border:2px solid black;text-align:center;font-weight:bold;color:black;" value="1" />
+					<input type="text" autocomplete="off" name="identifier" maxlength="255" style="width:120px;border:2px solid black;text-align:center;font-weight:bold;color:black;" value="" />
 				</span>
 			</div>
 			<div style="clear:left;padding-top:6px;float:left;">