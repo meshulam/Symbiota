e76fc324f3ea271e8ec8e0acfc4b599f65cf9347
Matthew Meshulam <meshu003@umn.edu>
2025-04-10 14:49:05 -0500

    Fix UNION syntax to be compatible with MariaDB 10.3

--- classes/TaxonProfile.php
+++ classes/TaxonProfile.php
@@ -454,13 +454,13 @@ class TaxonProfile extends Manager {
 	private function setLinkArr(){
 		if($this->linkArr === false && $this->tid){
 			$this->linkArr = array();
-			$sql = '(SELECT tlid, url, icon, title, notes, sortsequence
+			$sql = 'SELECT tlid, url, icon, title, notes, sortsequence
 				FROM taxalinks l
 				WHERE (l.tid = ' . $this->tid . ')
 				UNION
 				SELECT l.tlid, l.url, l.icon, l.title, l.notes, l.sortsequence
 				FROM taxalinks l INNER JOIN taxaenumtree e ON l.tid = e.parenttid
-				WHERE (e.tid = ' . $this->tid . '))
+				WHERE (e.tid = ' . $this->tid . ')
 				ORDER BY sortsequence, title';
 			$rs = $this->conn->query($sql);
 			while($r = $rs->fetch_object()){
