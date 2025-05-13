1bee0360c2b63fbfcc1aa5c9d69d9ddcbd4af2b7
Matthew Meshulam <meshu003@umn.edu>
2025-05-13 15:17:18 -0500

    Update styles to better reflect Bell Museum branding
    - Switches font to fira sans
    - Brings in new header image
    - Removes search and links from UMN header, if we want a site search there it should be Atlas-specific
    - Improve header styles

--- includes/header.php
+++ includes/header.php
@@ -7,28 +7,26 @@ $SHOULD_USE_HARVESTPARAMS = $SHOULD_USE_HARVESTPARAMS ?? false;
 $collectionSearchPage = $SHOULD_USE_HARVESTPARAMS ? '/collections/index.php' : '/collections/search/index.php';
 ?>
 <div class="header-wrapper">
-	<div class="umnhf" id="umnhf-h" role="banner">
-		<a class="screen-reader-only" href="#end-nav"><?= $LANG['H_SKIP_NAV'] ?></a>
-		<div class="printer"><div class="left"></div><div class="right"><strong>University of Minnesota</strong><br />https://www.umn.edu/<br />612-625-5000</div></div>
-		<div class="umnhf" id="umnhf-h-mast">
-			<a class="umnhf" id="umnhf-h-logo" href="https://twin-cities.umn.edu/"><span>Go to the U of M home page</span></a>
-			<ul class="umnhf" id="umnhf-h-ql">
-				<li><a href="http://onestop.umn.edu/">One Stop</a></li>
-				<li class="umnhf"><a href="https://www.myu.umn.edu/">MyU <span></span>: For Students, Faculty, and Staff</a></li>
-			</ul>
-			<!-- Button below is for dropdown toggle, only visible on mobile screens. If using
-			a non-dropdown version you can delete this tag -->
-			<button class="umnhf" id="umnhf-m-search">Search</button>
+	<div class="umnhf-wrapper">
+		<!-- BEGIN U of M HEADER -->
+		<div class="umnhf container" id="umnhf-h">
+			<!-- Skip Links: Give your nav and content elements the appropriate ID attributes -->
+			<div id="skipLinks" class="screen-reader-only"><a href="#end-nav"><?= $LANG['H_SKIP_NAV'] ?></a></div>
+			<div class="printer"><div class="left"></div><div class="right"><strong>University of Minnesota</strong><br>https://twin-cities.umn.edu/<br>612-625-5000</div></div>
+			<div class="umnhf" id="umnhf-h-mast">
+				<a class="umnhf" id="umnhf-h-logo" href="https://twin-cities.umn.edu/">
+					<span class="screen_reader_only">Go to the U of M home page</span>
+				</a>
+			</div>
 		</div>
-		<form class="umnhf" id="umnhf-h-search" action="https://www.bellmuseum.umn.edu/" method="get" title="Search the Bell Museum Website" role="search">
-			<label class="umnhf" for="umnhf-h-st" for="s">Search the Bell Museum Website</label>
-			<input class="umnhf" id="umnhf-h-st" type="text" name="s" id="s" value="" placeholder="search the bell">
-			<input aria-hidden="true" type="hidden" name="search-type" value="normal">
-			<input class="umnhf" id="umnhf-h-sb" type="submit" value="">
-		</form>
 	</div>
 	<header>
 		<div class="top-wrapper">
+			<div class="top-brand">
+				<div class="brand-name">
+					<h1><a href="<?= $CLIENT_ROOT ?>">Minnesota Biodiversity Atlas</a></h1>
+				</div>
+			</div>
 			<nav class="top-login" aria-label="horizontal-nav">
 				<?php
 				if ($USER_DISPLAY_NAME) {
@@ -36,34 +34,23 @@ $collectionSearchPage = $SHOULD_USE_HARVESTPARAMS ? '/collections/index.php' : '
 					<div class="welcome-text">
 						<?= $LANG['H_WELCOME'] . ' ' . $USER_DISPLAY_NAME ?>!
 					</div>
-					<span id="profile">
-						<form name="profileForm" method="post" action="<?= $CLIENT_ROOT . '/profile/viewprofile.php' ?>">
-							<button class="button button-tertiary left-breathing-room-rel" name="profileButton" type="submit"><?= $LANG['H_MY_PROFILE'] ?></button>
-						</form>
-					</span>
-					<span id="logout">
-						<form name="logoutForm" method="post" action="<?= $CLIENT_ROOT ?>/profile/index.php?submit=logout">
-							<button class="button button-secondary left-breathing-room-rel" name="logoutButton" type="submit"><?= $LANG['H_LOGOUT'] ?></button>
-						</form>
-					</span>
+					<form id="profile" name="profileForm" method="post" action="<?= $CLIENT_ROOT . '/profile/viewprofile.php' ?>">
+						<button class="button button-tertiary left-breathing-room-rel" name="profileButton" type="submit"><?= $LANG['H_MY_PROFILE'] ?></button>
+					</form>
+					<form id="logout" name="logoutForm" method="post" action="<?= $CLIENT_ROOT ?>/profile/index.php?submit=logout">
+						<button class="button button-secondary left-breathing-room-rel" name="logoutButton" type="submit"><?= $LANG['H_LOGOUT'] ?></button>
+					</form>
 					<?php
 				} else {
 					?>
-					<span id="login">
-						<form name="loginForm" method="post" action="<?= $CLIENT_ROOT . "/profile/index.php" ?>">
-							<input name="refurl" type="hidden" value="<?= htmlspecialchars($_SERVER['SCRIPT_NAME'], ENT_COMPAT | ENT_HTML401 | ENT_SUBSTITUTE) . "?" . htmlspecialchars($_SERVER['QUERY_STRING'], ENT_QUOTES) ?>">
-							<button class="button button-secondary left-breathing-room-rel" name="loginButton" type="submit"><?= $LANG['H_LOGIN'] ?></button>
-						</form>
-					</span>
+					<form id="login" name="loginForm" method="post" action="<?= $CLIENT_ROOT . "/profile/index.php" ?>">
+						<input name="refurl" type="hidden" value="<?= htmlspecialchars($_SERVER['SCRIPT_NAME'], ENT_COMPAT | ENT_HTML401 | ENT_SUBSTITUTE) . "?" . htmlspecialchars($_SERVER['QUERY_STRING'], ENT_QUOTES) ?>">
+						<button class="button button-secondary left-breathing-room-rel" name="loginButton" type="submit"><?= $LANG['H_LOGIN'] ?></button>
+					</form>
 					<?php
 				}
 				?>
 			</nav>
-			<div class="top-brand">
-				<div class="brand-name">
-					<h1><a href="<?= $CLIENT_ROOT ?>">Minnesota Biodiversity Atlas</a></h1>
-				</div>
-			</div>
 		</div>
 		<div class="menu-wrapper">
 			<!-- Hamburger icon -->
